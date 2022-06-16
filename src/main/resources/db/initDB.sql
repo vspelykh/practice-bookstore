12cd337126d779c54c2895b6792f5ca0382b42ca
Vladyslav Pelykh <vspelykh@gmail.com>
2022-06-16 23:10:32 +0300

    Fix initDB.sql, liquibase init schema: useful for all DB
    Signed-off-by: Vladyslav Pelykh <vspelykh@gmail.com>

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -6,27 +6,15 @@ DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS languages;
 DROP TABLE IF EXISTS covers;
 
-DROP SEQUENCE IF EXISTS global_seq;
-DROP SEQUENCE IF EXISTS categories_seq;
-DROP SEQUENCE IF EXISTS sub_categories_seq;
-DROP SEQUENCE IF EXISTS languages_seq;
-DROP SEQUENCE IF EXISTS covers_seq;
-
-CREATE SEQUENCE global_seq START WITH 1;
-CREATE SEQUENCE categories_seq START WITH 1;
-CREATE SEQUENCE sub_categories_seq START WITH 1;
-CREATE SEQUENCE languages_seq START WITH 1;
-CREATE SEQUENCE covers_seq START WITH 1;
-
-
 CREATE TABLE categories
 (
-    id       INTEGER PRIMARY KEY DEFAULT nextval('categories_seq'),
+    id       SERIAL PRIMARY KEY,
     category VARCHAR UNIQUE NOT NULL
 );
+
 CREATE TABLE sub_categories
 (
-    id           INTEGER PRIMARY KEY DEFAULT nextval('sub_categories_seq'),
+    id           SERIAL PRIMARY KEY,
     category_id  INTEGER        NOT NULL,
     sub_category VARCHAR UNIQUE NOT NULL,
     FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
@@ -34,31 +22,31 @@ CREATE TABLE sub_categories
 
 CREATE TABLE publishers
 (
-    id        INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    id        SERIAL PRIMARY KEY,
     publisher VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE authors
 (
-    id     INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    id     SERIAL PRIMARY KEY,
     author VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE languages
 (
-    id       INTEGER PRIMARY KEY DEFAULT nextval('languages_seq'),
+    id       SERIAL PRIMARY KEY,
     language VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE covers
 (
-    id   INTEGER PRIMARY KEY DEFAULT nextval('covers_seq'),
+    id   SERIAL PRIMARY KEY,
     type VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE books
 (
-    id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    id              SERIAL PRIMARY KEY,
     vendor_code     INTEGER NOT NULL,
     title           VARCHAR NOT NULL,
     author_id       INTEGER NOT NULL,
@@ -67,7 +55,7 @@ CREATE TABLE books
     sub_category_id INTEGER NOT NULL,
     language_id     INTEGER NOT NULL,
     cover_id        INTEGER NOT NULL,
-    year            INTEGER NOT NULL,
+    "year"          INTEGER NOT NULL,
     price           DECIMAL NOT NULL,
     cover_image_url VARCHAR NOT NULL,
     FOREIGN KEY (author_id) REFERENCES authors ON DELETE CASCADE,
@@ -78,3 +66,65 @@ CREATE TABLE books
 );
 
 CREATE UNIQUE INDEX books_unique_author_title_idx ON books (author_id, title);
+
+INSERT INTO categories (category)
+VALUES ('Health'),
+       ('Business'),
+       ('Computing'),
+       ('Romance'),
+       ('Religion'),
+       ('Sport'),
+       ('Fantasy'),
+       ('Thriller');
+
+INSERT INTO sub_categories(sub_category, category_id)
+VALUES ('Medicine', 1),
+       ('Animals', 1),
+       ('Sales', 2),
+       ('Leadership', 2),
+       ('Math', 3),
+       ('Physics', 3),
+       ('Historical', 4),
+       ('Romantic', 4),
+       ('Modern', 5),
+       ('Ancient', 5),
+       ('Football', 6),
+       ('Tennis', 6),
+       ('Low', 7),
+       ('Magic', 7),
+       ('Crime', 8),
+       ('Psychological', 8);
+
+INSERT INTO publishers (publisher)
+VALUES ('Ranok Publishing House Ltd.'),
+       ('Svichado Publishers Ltd.'),
+       ('Znannia Publishing House');
+
+INSERT INTO authors (author)
+VALUES ('Samuel Shem'),
+       ('Jerome Groopman'),
+       ('Emma Jane Unsworth'),
+       ('John Brooks'),
+       ('Brené Brown'),
+       ('Agatha Christie'),
+       ('Sir Arthur Conan Doyle');
+
+INSERT INTO languages (language)
+VALUES ('Ukrainian'),
+       ('English'),
+       ('Italian');
+
+INSERT INTO covers (type)
+VALUES ('Hard'),
+       ('Soft');
+--
+INSERT INTO books (vendor_code, title, author_id, publisher_id, pages, sub_category_id,
+                   language_id, cover_id, "year", price, cover_image_url)
+VALUES ('1', 'Morphology of Humans', 3, 1, 700, 1, 1, 2, 2015, 500, 'random_url1'),
+       ('2', 'Outliers: The Story of Success', 4, 3, 250, 5, 2, 1, 2020, 999, 'random_url2'),
+       ('3', 'It Ends With Us', 6, 2, 450, 8, 1, 1, 2010, 250, 'random_url3'),
+       ('4', 'The Confessions', 5, 3, 550, 5, 2, 1, 2009, 350, 'random_url4'),
+       ('5', 'Mohawk Saint: Catherine Tekakwitha and the Jesuits', 5, 3, 510, 6, 2, 1, 2009, 350, 'random_url5'),
+       ('6', 'Moneyball: The Art of Winning', 1, 3, 690, 11, 2, 2, 2000, 1999, 'random_url6'),
+       ('7', 'The Body in the Library: A Puzzle', 6, 3, 789, 16, 2, 1, 1970, 2000, 'random_url7'),
+       ('8', 'The Silent Patient', 7, 1, 550, 15, 1, 1, 1995, 666, 'random_url8');

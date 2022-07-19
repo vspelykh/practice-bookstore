2ea0f29e7b03e5295caa73c87d2c28eee61eaa05
Vladyslav Pelykh <vspelykh@gmail.com>
2022-07-19 13:05:04 +0300

    Add error pages

---
+++ src/main/java/ua/hillel/bookstore/web/BookstoreErrorController.java
@@ -0,0 +1,28 @@
+package ua.hillel.bookstore.web;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class BookstoreErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error/500";
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error/403";
+            }
+        }
+        return "error/404";
+    }
+}
